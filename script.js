4df579c66683a824f676dbc7b926a8f595db6e6e
alessandrogammieri <alessandrogammieri.it@gmail.com>
2019-07-03 17:43:26 +0200

    last name list

--- script.js
+++ script.js
@@ -5,6 +5,23 @@ Scrivi a che posizione della lista il nuovo utente si trova. */
 
 
 // Dichiarazione delle variabili
-console.log("Ciao Mondo!");
+var cognome, tuocognome, list, elenco;
 
-// Invocazione delle variabili
+// Lista dei nostri cognomi
+list = ["Rossi", "Bianchi", "Carli", "Visco"];
+
+// id elenco
+elenco = document.getElementById('mioid');
+
+// Chiedi all'utente il cognome
+tuocognome = prompt("Inserisci il tuo cognome");
+list.push(tuocognome);
+
+// Stampa la lista dei cognomi
+for (var i = 0; i < list.length; i++) {
+  cognome = elenco.innerHTML;
+  elenco.innerHTML = cognome + "<li>" + list.sort()[i] + "</li>";
+}
+
+// Comunicare la posizione nella Lista
+document.getElementById('di').innerHTML = "Il tuo cognome è in posizione " + (list.indexOf(tuocognome) + 1);
